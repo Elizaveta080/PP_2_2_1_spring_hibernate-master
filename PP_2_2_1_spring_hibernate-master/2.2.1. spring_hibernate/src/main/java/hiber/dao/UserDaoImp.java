8c0c9eb2bdcd04a9a6125043f342905dda3d4f5c
Elizaveta080 <elizavetamelkozerova00@gmail.com>
2023-02-16 21:21:34 +0300

    Initial commit

---
+++ PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,52 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+
+   private final SessionFactory sessionFactory;
+
+
+   public UserDaoImp(SessionFactory sessionFactory) {
+      this.sessionFactory = sessionFactory;
+   }
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   public void add(Car car) {
+      sessionFactory.getCurrentSession().save(car);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<Car> listCars() {
+      TypedQuery<Car> query=sessionFactory.getCurrentSession().createQuery("from Car");
+      return query.getResultList();
+   }
+
+   public User findUser(String model, int series) {
+      TypedQuery<User> findUser = sessionFactory.getCurrentSession().createQuery("from User as us where us.car.model =:model and us.car.series =:series")
+              .setParameter("model", model)
+              .setParameter("series", series);
+      return findUser.getSingleResult();
+   }
+}
---
+++ PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,77 @@
+package hiber.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "Cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long car_id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public Car() {
+
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getCar_id(){
+        return car_id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setCar_id(Long car_id) {
+        this.car_id = car_id;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+    @Override
+    public String toString() {
+        return "Car{" +
+                "series=" + series +
+                ", user=" + user +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return getSeries() == car.getSeries() && Objects.equals(getCar_id(), car.getCar_id()) && Objects.equals(getModel(), car.getModel()) && Objects.equals(user, car.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCar_id(), getModel(), getSeries(), user);
+    }
+
+}
